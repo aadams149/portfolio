187b1754530f749530629b515d9c10ad8e8889a1
Alex Adams <aja149@georgetown.edu>
2021-01-08 15:59:47 -0500

    COVID County, USA
    This is the app.R file for my first R Shiny Web App, which I'm calling COVID County, USA.

--- Personal_projects/COVID_County_USA/app.R
+++ Personal_projects/COVID_County_USA/app.R
@@ -4,16 +4,15 @@ library(ggplot2)
 library(dplyr)
 library(usmap)
 library(png)
-library(cairoDevice)
 library(Cairo)
-
+library(stringr)
 options(bitmapType='cairo')
 
 #Import covid deaths spreadsheet
 urlfile = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"
 c19data = read.csv(url(urlfile))
 
-countydata1 = read.csv('countydata1.csv')
+countydata1 = read.csv('countydata1.csv', stringsAsFactors = FALSE, sep = ',', header = TRUE)
 countydata1$FIPS = sprintf("%05d", as.numeric(countydata1$FIPS))
 justfips = countydata1[,c('FIPS', 'TOT_POP')]
 justfips = justfips %>% rename(fips = FIPS)
@@ -26,16 +25,16 @@ ui <- fluidPage(
   titlePanel(h1('COVID County, USA', align = 'center')),
   
   hr(),
- 
+  
   helpText(h3('The COVID-19 pandemic has upended life as we know it. 
   It can be hard to fully understand the magnitude of the pandemic, particularly in the 
   United States, which has lost more people than any other country. 
   This app helps visualize the impact of the COVID-19 pandemic.'),
-  h3('First, pick a date using the calendar widget below.
+           h3('First, pick a date using the calendar widget below.
       The app will then create a map of the United States at the county level.'),
-  h3('Counties shaded in red have a smaller population than the cumulative U.S. COVID-19 death total
+           h3('Counties shaded in red have a smaller population than the cumulative U.S. COVID-19 death total
                        as of the specified date.'),
-  h3('Counties shaded in green have a larger population than the cumulative COVID-19 death total.'), align = 'center'),
+           h3('Counties shaded in green have a larger population than the cumulative COVID-19 death total.'), align = 'center'),
   helpText('Copyright 2020, Alexander J. Adams', align = 'center'),
   tags$h6('Data:', align = 'left'),
   tags$a(href = 'https://github.com/nytimes/covid-19-data', 'The New York Times, '),
